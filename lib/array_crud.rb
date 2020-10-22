31f713b118f003e9fff6a6acae608d556ec51693
Colton O'Connor <colton.shawn.oconnor@gmail.com>
2020-10-22 04:04:58 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -20,7 +20,8 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  
+  removed_element = array.shift
+  array
 end
 
 def retrieve_element_from_index(array, index_number)
